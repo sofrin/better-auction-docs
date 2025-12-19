948252e48c74b37f95fcb91f4b08963c51a46e3e
Sofrin <85531415+sofrin@users.noreply.github.com>
2025-12-19 21:15:56 +0700

    upd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,46 @@
-import { RootProvider } from 'fumadocs-ui/provider/next';
-import './global.css';
-import { Inter } from 'next/font/google';
+import { RootProvider } from "fumadocs-ui/provider/next";
+import "./global.css";
+import { DocsLayout } from "fumadocs-ui/layouts/docs";
+import { Inter } from "next/font/google";
+import { baseOptions } from "@/lib/layout.shared";
+import { source } from "@/lib/source";
+import type { Metadata } from 'next'
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin", "cyrillic"],
 });
 
-export default function Layout({ children }: LayoutProps<'/'>) {
+
+export const metadata: Metadata = {
+  title: 'Better-Auction Docs',
+  description: "Документация по сайту Better-Auction",
+}
+
+export default function Layout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="ru" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
-        <RootProvider>{children}</RootProvider>
+        <RootProvider
+          i18n={{
+            locale: "ru",
+            translations: {
+              chooseTheme: "Выбор темы",
+              nextPage: "Следующая страница",
+              editOnGithub: "Редактировать на Github",
+              toc: "Оглавление",
+              lastUpdate: "Последнее изменение",
+              search: "Поиск",
+              tocNoHeadings: "Нет заголовков",
+              previousPage: "Предыдущая страница",
+              searchNoResult: "Ничего не найдено",
+              chooseLanguage: "Выбор языка",
+            },
+          }}
+        >
+          <DocsLayout tree={source.pageTree} {...baseOptions()}>
+            {children}
+          </DocsLayout>
+        </RootProvider>
       </body>
     </html>
   );
---
+++ src/lib/i18n.ts
@@ -0,0 +1,7 @@
+import { defineI18n } from 'fumadocs-core/i18n';
+
+export const i18n = defineI18n({
+  defaultLanguage: 'ru',
+  languages: ['ru'],
+  hideLocale:"always"
+});
