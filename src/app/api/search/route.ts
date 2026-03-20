e45ba67830b2a758b29a58f8c96e05bcd1788089
Sofrin <85531415+sofrin@users.noreply.github.com>
2026-03-20 23:26:30 +0700

    upd

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -3,5 +3,5 @@ import { source } from "@/lib/source";
 
 export const { GET } = createFromSource(source, {
   // https://docs.orama.com/docs/orama-js/supported-languages
-  language: "Russian",
+  language: "russian",
 });
